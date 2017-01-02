efb1e356665d8b97c43658f45db73219fdb63cf1
A.Fairiz <afairiz@gmail.com>
2017-01-02 11:23:14 +0800

    Initial Commit

---
+++ Word Scramble/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Word Scramble
+//
+//  Created by Ahmad Fairiz on 02/01/2017.
+//  Copyright © 2017 Recite Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
